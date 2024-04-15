c6756a3e2ed0efff5b9d22af3dcdde0f86afb5d0
Daniel John <dcampo85@gmail.com>
2024-04-15 18:33:01 -0400

    start work on reaction.js

---
+++ models/Reaction.js
@@ -0,0 +1,7 @@
+const { Schema, Types } = require("mongoose");
+const reactionSchema = new Schema(
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+        
+)
