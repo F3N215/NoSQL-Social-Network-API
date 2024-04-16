b73b7e42b5204abf38cd8b16e6d20046ab1f93f3
Daniel John <dcampo85@gmail.com>
2024-04-16 17:16:02 -0400

    troubleshooting

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 // Connecting to the MongoDB database using the MongoDB URI provided in the environment
 // variables or using the default URI if the environment variable is not set
 mongoose.connect(
-  process.env.MONOGDB_URI || "mongodb://localhost:3001/bits&bytes",
+  process.env.MONOGDB_URI || "mongodb://localhost:27017/Bits&Bytes",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 // middlewares
-app.use(expressurlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(routes); // using routes from routes.js
