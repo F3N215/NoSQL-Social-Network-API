2f72abb779979504abb7ec761a93c5bbf8c43461
Daniel John <dcampo85@gmail.com>
2024-04-16 14:09:24 -0400

    updating routes

---
+++ routes/api-routes/byte-routes.js
@@ -0,0 +1,25 @@
+// start with importing controllers & dependencies
+const route = require("express").Router();
+const {
+  getAllBytes,
+  getBytesById,
+  createByte,
+  deleteByte,
+  updateByteById,
+  createReaction,
+  deleteReaction,
+} = require("../../controllers/byte-controller");
+
+router.route("/").get(getAllBytes).post(createByte); // GET and POST for all bytes
+
+route
+  .route("/:byteId")
+  .get(getBytesById)
+  .put(updateByteById)
+  .delete(deleteByte); // GET, PUT and DELETE for a single byte
+
+route.route("/:byteId/reactions").post(createReaction); // POST for reactions to a byte
+
+route.route("/:byteId/reactions/:reactionId").delete(deleteReaction); // DELETE for a reaction to a byte
+
+module.exports = router;
--- routes/api-routes/thought-routes.js
+++
@@ -1,6 +0,0 @@
-// start with importing controllers & dependencies
-const route = require('express').Router();
-const {
-    getAllThoughts,
-    getThoughtById,
-    create
