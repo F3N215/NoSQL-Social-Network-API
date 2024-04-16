69965168e0843269a2d7d7c9e9ab95685075e705
Daniel John <dcampo85@gmail.com>
2024-04-16 16:49:15 -0400

    add middlewares

--- server.js
+++ server.js
@@ -2,7 +2,17 @@ const express = require("express");
 const db = require("./config/connection");
 const routes = require("./routes");
 
+// server setup: express and environment variables
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // middlewares
+app.use(expressurlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(routes); // using routes from routes.js
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server now running on port ${PORT}!`);
+  });
+});
